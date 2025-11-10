c8db19569964a1fb7fa818a59226ecc097ad5cff
Shiisawa Sakino <sakino-shiisawa@gmail.com>
2025-11-10 15:45:53 +0900

    add fatal error to __all__

--- modules/parsekit/parsekit/__init__.py
+++ modules/parsekit/parsekit/__init__.py
@@ -7,6 +7,6 @@ from .combinator import *
 
 __all__ = [
 	"tokenize", "Token", "TokenConstructor",
-	"Node", "RepeatNode", "SequenceNode", "ChoiceNode", "PackNode", "ParseError",
+	"Node", "RepeatNode", "SequenceNode", "ChoiceNode", "PackNode", "FatalNode", "ParseError", "FatalParseError",
 	"ParseResult", "ParseReturn",
 ]
